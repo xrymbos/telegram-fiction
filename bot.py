d0378a9db50a5a65571f8e75f00c0cc603bb15fa
Jamie McCloskey <crimbo1994@gmail.com>
2016-01-12 21:16:11 +1100

    working commands

--- bot.py
+++ bot.py
@@ -5,7 +5,6 @@ import json
 from twisted.web import client
 from twisted.internet import reactor, defer
 
-offset = 0
 token = "173353654:AAG3Xxh92Aei4IP5ofTLohZitLFk4qv08YM"
 
 def handleMessageResponse(results):
@@ -19,14 +18,15 @@ def reply(chat_id, message):
     url = "https://api.telegram.org/bot{0}/sendMessage?{1}".format(token, param_string)
     print("replying")
     client.getPage(url).addCallback(handleMessageResponse)
-    reactor.run()
     
-def handleUpdate(results):
+def handleUpdate(results, offset):
     print("finished polling.")
     parsed_results = json.loads(results) 
     if not parsed_results['ok']:
-        continue
+        print("error: {0}".format(results))
+        return
     for result in parsed_results['result']:
+        print("processing result {0}", result)
         if not 'message' in result:
             continue
         message = result['message']
@@ -35,19 +35,22 @@ def handleUpdate(results):
         text = message['text']
         print(text)
         reply(82066642, text)
+        print("done replying")
         if not 'update_id' in result:
+            print("continuing")
             continue
         if result['update_id'] >= offset:
             offset = result['update_id'] + 1
+    print("updated offset is {0}".format(offset))
+    checkForUpdates(offset)
     
-def checkForUpdates:
+def checkForUpdates(offset):
     print("polling...")
     params = { 'timeout' : 1000, 'offset' : offset }
     param_string = urllib.urlencode(params)
     url = "https://api.telegram.org/bot{0}/getUpdates?{1}".format(token, param_string)
     print(url)
-    results = urllib2.urlopen(url).read()
-    client.getPage(url).addCallback(handleUpdate)
+    client.getPage(url).addCallback(handleUpdate, offset)
 
-checkForUpdates()
+checkForUpdates(0)
 reactor.run()
